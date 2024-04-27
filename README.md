98cce27ecd0bd6cf3af2c240655c6fa5d403bccb
viniciusdutra314 <dutravinisousa2@usp.br>
2024-04-27 14:23:52 -0300

    dummy deck created

---
+++ README.md
@@ -0,0 +1,15 @@
+# Showcase
+
+### On the fly:
+
+
+# Installation
+The most reliable way of installing a template on anki is importing a deck with cards created using the template, in order to do that the we gonna run a Google Colab script that will generate the deck  for us, please click this link and follow the inscrutions
+
+
+(if you're a developer you can run the create_template.py by yourself)
+
+### Support on Linux:
+Unfortunately, Linux doesn't come with TTS by default like the other operational systems, one easy solution is to install the anki-addon [AwesomeTTS](https://ankiweb.net/shared/info/1436550454), it will generate audios using online services like Google Translate, you need to create a preset selecting a service/voice for your target language and this is it.
+
+The audios will continue to be generated on the fly and other voices will be avaiable on your different devices
--- create_template.py
+++ create_template.py
@@ -1,22 +1,33 @@
 def html_replacing(cardtype: str,config:dict) -> list[str]:
     from os.path import join as path_join
     htmls=[]
-    for side in ["front.html","back.html"]:
+    for side in ["Front.html","Back.html"]:
         with open(path_join("directory",cardtype,side),"r") as file:
             html = file.read()
         for key,value in config.items():
             html=html.replace(key,value)   
         htmls.append(html)
-    return {"front":htmls[0],"back":htmls[1]}
+    return {"Front":htmls[0],"Back":htmls[1]}
 
 
 def main(your_lang :str ,target_lang :str ,
         highlight_color :str,cardtypes:list[str],
-        additional_voices:list[str]=[]):
+        template_name:str, additional_voices:list[str]=[]):
     import langcodes
+    import genanki
+    import random
     FIELDS=("🔤 Phrase","🔄 Translated phrase or word",
                 "❓ Unknown word","🗣️ Pronunciation")
     LINUX_SUPPORT="AwesomeTTS"
+    CSS=""".card {
+            font-family: arial;
+            font-size: 20px;
+            text-align: center;
+            color: black;
+            background-color: white;
+            }
+        """
+
     #checking
     if not langcodes.get(your_lang).is_valid():
         print(f"You typed the code-lang of your language incorrectly")
@@ -35,7 +46,36 @@ def main(your_lang :str ,target_lang :str ,
     config={"your_voices":your_voices,"your_lang":your_lang,
             "target_lang":target_lang,"highlight_color":highlight_color,
             "dictionary_link":dictionary_link}
-    cardtype_templates={
+    cardtype_htmls={
             cardtype:html_replacing(cardtype,config) for cardtype in cardtypes}
+    cardtypes_templates=[]
+    for cardtype in cardtype_htmls:
+        cardtypes_templates.append(
+            {"name":cardtype,
+            "qfmt":cardtype_htmls[cardtype]["Front"],
+            "afmt":cardtype_htmls[cardtype]["Back"],
+            })
+    note_template=genanki.Model(
+        random.randrange(1<<30,1<<31),
+        template_name,
+        fields=[
+            {"name":FIELDS[0]},{"name":FIELDS[1]},
+            {"name":FIELDS[2]},{"name":FIELDS[3]}
+        ],
+        templates=cardtypes_templates,
+        css=CSS
+    )
+    dummy_deck=genanki.Deck(
+        random.randrange(1<<30,1<<31),
+        "Dummy Deck (just to import)"
+    )
+    dummy_note=genanki.Note(
+        model=note_template,
+        fields=["This is a dummy phrase",
+        "an artificial substitute looking like the real thing",
+        "dummy","/ˈdʌm·i/"]
+    )
+    dummy_deck.add_note(dummy_note)
+    genanki.Package(dummy_deck).write_to_file("exemplo.apkg")
 if __name__=="__main__":
-    main("en_US","it_IT","green",["Passive","Active"])
+    main("en_US","it_IT","green",["Passive","Active"],"awesome-features")
